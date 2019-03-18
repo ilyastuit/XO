1dba8531a619fb63b9f308a79866ca0a94f982e7
ilyastuit <ilyastuit@gmail.com>
2019-03-19 03:42:13 +0500

    Field - created

---
+++ src/ilyas.xo/Field.java
@@ -0,0 +1,23 @@
+package ilyas.xo;
+
+import java.awt.*;
+
+public class Field {
+
+    private final static int FIELD_SIZE = 3;
+
+    private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
+
+    public int getSize() {
+        return FIELD_SIZE;
+    }
+
+    public Figure getFigure(final Point point) {
+        return field[point.x][point.y];
+    }
+
+    public void setFigure(final Point point, final Figure figure) {
+        field[point.x][point.y] = figure;
+    }
+
+}
