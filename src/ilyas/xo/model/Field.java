becb611176f41b2fe85fa64473c9b917a34d8a50
ilyastuit <ilyastuit@gmail.com>
2019-03-19 12:53:01 +0500

    Field - tests for the Point added

--- src/ilyas/xo/model/Field.java
+++ src/ilyas/xo/model/Field.java
@@ -1,5 +1,8 @@
 package ilyas.xo.model;
 
+import ilyas.xo.model.exceptions.AlreadyOccupiedException;
+import ilyas.xo.model.exceptions.InvalidePointException;
+
 import java.awt.*;
 
 public class Field {
@@ -14,14 +17,30 @@ public class Field {
         return FIELD_SIZE;
     }
 
-    public Figure getFigure(final Point point) {
+    public Figure getFigure(final Point point) throws InvalidePointException {
+        if (!checkPoint(point)) {
+            throw new InvalidePointException();
+        }
         return field[point.x][point.y];
     }
 
-    public void setFigure(final Point point, final Figure figure) {
+    public void setFigure(final Point point, final Figure figure)  throws InvalidePointException,
+                                                                        AlreadyOccupiedException{
+        if (!checkPoint(point)) {
+            throw new InvalidePointException();
+        }
+        if (field[point.x][point.y] != null) {
+            throw new AlreadyOccupiedException();
+        }
         field[point.x][point.y] = figure;
     }
 
+    private boolean checkPoint(final Point point) {
+        return checkCoordinate(point.x) && checkCoordinate(point.y);
+    }
 
+    private boolean checkCoordinate(final int coordinate) {
+        return coordinate >= MIN_COORDINATE && coordinate < MAX_COORDINATE;
+    }
 
 }
